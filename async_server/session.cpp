e39b7d37e8588c9c35137fd6c83923ae026b4557
paradise2017 <1131766738@qq.com>
2025-06-04 23:03:21 +0800

    伪闭包延长连接的生命周期,（一种智能指针新写法）

--- async_server/session.cpp
+++ async_server/session.cpp
@@ -1,7 +1,11 @@
 #include "session.hpp"
-Session::Session(boost::asio::io_context &ioc)
+Session::Session(boost::asio::io_context &ioc, Server *server)
     : socket_(ioc)
 {
+    // 借助 Boost 库生成随机 UUID，并将其转换为字符串形式，存储在 Session 对象里，
+    // 方便后续对不同的会话进行唯一标识和管理。
+    boost::uuids::uuid temp_uuid = boost::uuids::random_generator()();
+    uuid_ = boost::uuids::to_string(temp_uuid);
 }
 
 tcp::socket &Session::Socket()
@@ -12,38 +16,48 @@ tcp::socket &Session::Socket()
 void Session::Start()
 {
     memset(data_, 0, max_length);
+
+    // shared_ptr<session>(this) 普通的智能指针，是一个局部对象 传递过去就是 shared_ptr<Session> self(this)
+    // 如何通过this 再生成一个智能指针，实现引用计数同步？
+    // shared_from_this() 返回指向自己的一个智能指针，实现引用计数同步
+
     socket_.async_read_some(boost::asio::buffer(data_, max_length),
                             std::bind(&Session::handle_read, this, placeholders::_1,
-                                      placeholders::_2));
+                                      placeholders::_2, shared_from_this()));
 }
 
-void Session::handle_read(const boost::system::error_code &error, std::size_t bytes_transferred)
+std::string Session::GetUuid()
+{
+    return uuid_;
+}
+
+void Session::handle_read(const boost::system::error_code &error, std::size_t bytes_transferred, shared_ptr<Session> self)
 {
     if (!error)
     {
         cout << "server receive data is " << data_ << endl;
         boost::asio::async_write(socket_, boost::asio::buffer(data_, bytes_transferred),
-                                 std::bind(&Session::handle_write, this, placeholders::_1));
+                                 std::bind(&Session::handle_write, this, placeholders::_1, self));
     }
     else
     {
         std::cout << "read error" << std::endl;
         // If an error occurs, we delete the session object.
-        delete this;
+        server_->ClearSession(uuid_);
     }
 }
 
-void Session::handle_write(const boost::system::error_code &error)
+void Session::handle_write(const boost::system::error_code &error, shared_ptr<Session> self)
 {
     if (!error)
     {
         memset(data_, 0, max_length);
-        socket_.async_read_some(boost::asio::buffer(data_, max_length), std::bind(&Session::handle_read, this, placeholders::_1, placeholders::_2));
+        socket_.async_read_some(boost::asio::buffer(data_, max_length), std::bind(&Session::handle_read, this, placeholders::_1, placeholders::_2, self));
     }
     else
     {
         std::cout << "write error" << std::endl;
-        delete this;
+        server_->ClearSession(uuid_);
     }
 }
 
@@ -55,26 +69,29 @@ Server::Server(boost::asio::io_context &ioc, short port)
     start_accept(); // Start accepting connections
 }
 
+void Server::ClearSession(std::string uuid)
+{
+    session_.erase(uuid);
+}
+
 void Server::start_accept()
 {
-    // shared_ptr<Session> new_session = make_shared<Session>(ioc_);
-    Session *new_session = new Session(ioc_); // Create a new session object
-    // Asynchronously accept a new connection
+    shared_ptr<Session> new_session = make_shared<Session>(ioc_, this);
     acceptor_.async_accept(new_session->Socket(),
                            std::bind(&Server::handle_accept, this, new_session, placeholders::_1));
 }
 
-void Server::handle_accept(Session *new_session, const boost::system::error_code &error)
+void Server::handle_accept(shared_ptr<Session> new_session, const boost::system::error_code &error)
 {
     if (!error)
     {
         cout << "new session accepted" << endl;
         new_session->Start();
-        start_accept(); // Start accepting the next connection
+        session_[new_session->GetUuid()] = new_session;
     }
     else
     {
         cout << "accept error: " << error.message() << endl;
-        delete new_session; // Clean up the session if there was an error
     }
+    start_accept();
 }
--- async_server/session.hpp
+++ async_server/session.hpp
@@ -1,19 +1,25 @@
 #pragma once
 #include <iostream>
 #include <boost/asio.hpp>
+#include <map>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 using boost::asio::ip::tcp;
 using namespace std;
+class Server;
 
-class Session
+// enable_shared_from_this模版类，enable_shared_from_this<Session> 模版类生成的真实类
+class Session : public std::enable_shared_from_this<Session>
 {
 public:
-    Session(boost::asio::io_context &ioc);
+    Session(boost::asio::io_context &ioc, Server *server);
     tcp::socket &Socket();
     void Start();
+    std::string GetUuid();
 
 private:
-    void handle_read(const boost::system::error_code &error, std::size_t bytes_transferred);
-    void handle_write(const boost::system::error_code &error);
+    void handle_read(const boost::system::error_code &error, std::size_t bytes_transferred, shared_ptr<Session> self);
+    void handle_write(const boost::system::error_code &error, shared_ptr<Session> self);
 
 private:
     tcp::socket socket_;
@@ -22,19 +28,23 @@ private:
         max_length = 1024
     };
     char data_[max_length];
+    Server *server_;
+    std::string uuid_;
 };
 
 class Server
 {
 public:
     Server(boost::asio::io_context &ioc, short port);
+    void ClearSession(std::string uuid);
 
 private:
     void start_accept();
-    void handle_accept(Session *new_session, const boost::system::error_code &error);
+    void handle_accept(shared_ptr<Session> new_session, const boost::system::error_code &error);
 
 private:
     // io_context 不允许构造
     boost::asio::io_context &ioc_;
     tcp::acceptor acceptor_;
+    std::map<std::string, shared_ptr<Session>> session_;
 };
